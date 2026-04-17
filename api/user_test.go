e39e38a6dc395e191b4f19fbe4d9664c393209ec
Xavier-Tame <xavs.tame@gmail.com>
2026-04-17 10:45:45 +0200

    fix: update user equality checks in TestCreateUser to compare individual fields

--- api/user_test.go
+++ api/user_test.go
@@ -364,5 +364,8 @@ func requireBodyMatchUser(t *testing.T, body *bytes.Buffer, user db.User) {
 	var gotUser db.User
 	err = json.Unmarshal(data, &gotUser)
 	require.NoError(t, err)
-	require.Equal(t, user, gotUser)
+	require.Equal(t, user.Username, gotUser.Username)
+	require.Equal(t, user.FullName, gotUser.FullName)
+	require.Equal(t, user.Email, gotUser.Email)
+	require.WithinDuration(t, user.CreatedAt, gotUser.CreatedAt, time.Second)
 }
